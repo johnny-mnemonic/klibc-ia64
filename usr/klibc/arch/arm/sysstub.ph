brainflux/klibc

commit bd56c54ae409a2c36d23e61d4075e2d1ffcd1a23

[klibc] ARM: More EABI and Thumb fixes.  Make the syscall stubs smaller.
More work on getting EABI and Thumb to reliably build.  It should all
be good now.

When building for EABI or Thumb, make the syscall stubs extra small by
using "bl" followed by an immediate; since we have already saved lr
the additional clobber of lr is not a problem.

Signed-off-by: H. Peter Anvin <hpa@zytor.com>

usr/klibc/arch/arm/sysstub.ph
@@ -18,11 +18,11 @@ sub make_sysstub($$$$$@) {
     print  OUT "	.globl	${fname}\n";
 
     print  OUT "#ifndef __thumb__\n";
-    print  OUT "	.balign	4\n";
 
     print  OUT "#ifndef __ARM_EABI__\n";
 
     # ARM version first
+    print  OUT "	.balign	4\n";
     print  OUT "${fname}:\n";
     print  OUT "	stmfd	sp!,{r4,r5,lr}\n";
     print  OUT "	ldr	r4,[sp,#12]\n";
@@ -33,35 +33,22 @@ sub make_sysstub($$$$$@) {
     print  OUT "#else /* __ARM_EABI__ */\n";
 
     # ARM EABI version
+    print  out "	.balign	4\n";
     print  OUT "${fname}:\n";
     print  OUT "	stmfd	sp!,{r4,r5,r7,lr}\n";
-    print  OUT "#if ARM_VALID_IMM(__NR_${sname})\n";
-    print  OUT "	mov	r7, # __NR_${sname}\n";
-    print  OUT "	b	__syscall_common\n";
-    print  OUT "#else\n";
-    print  OUT "	mov	r7, # (__NR_${sname} & 0xff00)\n";
-    print  OUT "	add	r7, r7, # (__NR_${sname} & 0xff)\n";
-    print  OUT "	b	__syscall_common\n";
-    print  OUT "#endif\n";
+    print  OUT "	bl	__syscall_common\n";
+    print  OUT "	.word	__NR_${sname}\n";
 
     print  OUT "#endif /* __ARM_EABI__ */\n";
     print  OUT "#else /* __thumb__ */\n";
 
     # Thumb version
-    print  OUT "	.balign	4\n";
+    print  OUT "	.balign	8\n";
     print  OUT "	.thumb_func\n";
     print  OUT "${fname}:\n";
     print  OUT "	push	{r4,r5,r7,lr}\n";
-    print  OUT "#if __NR_${sname} <= 0xff\n";
-    print  OUT "	mov	r7, # __NR_${sname}\n";
     print  OUT "	bl	__syscall_common\n";
-    print  OUT "#else\n";
-    print  OUT "	ldr	r7, 1f\n";
-    print  OUT "	bl	__syscall_common\n";
-    print  OUT "	.balign	4\n";
-    print  OUT "1:\n";
-    print  OUT "	.word	__NR_${sname}\n";
-    print  OUT "#endif\n";
+    print  OUT "	.short	__NR_${sname}\n";
 
     print  OUT "#endif /* __thumb__*/\n";
 

usr/klibc/arch/arm/MCONFIG
@@ -19,8 +19,14 @@ ifeq ($(CONFIG_KLIBC_THUMB),y)
 CPU_ARCH := $(CPU_ARCH)t
 KLIBCREQFLAGS += -mthumb
 KLIBCLDFLAGS  += --thumb-entry _start
-KLIBCREQFLAGS += -mabi=apcs-gnu
+KLIBCEMAIN     = --thumb-entry _start
+KLIBCREQFLAGS += -mabi=aapcs-linux
+KLIBCSHAREDFLAGS = -Ttext 0x380200
 else
+# Extra linkflags when building the shared version of the library
+# This address needs to be reachable using normal inter-module
+# calls, and work on the memory models for this architecture
+KLIBCSHAREDFLAGS = -Ttext 0x01800200
 ifeq ($(CONFIG_AEABI),y)
 KLIBCREQFLAGS += -mabi=aapcs-linux -mno-thumb-interwork
 else
@@ -28,7 +34,3 @@ KLIBCREQFLAGS += -mabi=apcs-gnu -mno-thumb-interwork
 endif
 endif
 
-# Extra linkflags when building the shared version of the library
-# This address needs to be reachable using normal inter-module
-# calls, and work on the memory models for this architecture
-KLIBCSHAREDFLAGS = -Ttext 0x01000200

usr/klibc/arch/arm/Makefile.inc
@@ -8,6 +8,7 @@
 #
 
 KLIBCARCHOBJS = \
+	arch/arm/setjmp.o \
 	arch/arm/syscall.o \
 	arch/arm/vfork.o \
 	arch/arm/aeabi_nonsense.o \
@@ -17,28 +18,6 @@ KLIBCARCHOBJS = \
 	libgcc/__udivdi3.o \
 	libgcc/__umoddi3.o \
 	libgcc/__udivmoddi4.o \
+	libgcc/__clzsi2.o \
+	libgcc/__clzdi2.o \
 
-ifeq ($(CONFIG_KLIBC_THUMB),y)
-KLIBCLIBGCC   :=
-KLIBCARCHOBJS += arch/arm/setjmp-thumb.o \
-	arch/arm/__muldi3.o \
-	libgcc/__divsi3.o \
-	libgcc/__modsi3.o \
-	libgcc/__udivsi3.o \
-	libgcc/__umodsi3.o \
-
-else
-KLIBCARCHOBJS += arch/arm/setjmp-arm.o
-endif
-
-arch/arm/sysstubs.a: arch/arm/sysstubs.pl
-	mkdir -p arch/arm/sysstubs
-	$(PERL) $< arch/arm/sysstubs
-	$(MAKE) $(patsubst %.S,%.o,$(wildcard arch/arm/sysstubs/*.S))
-	-rm -f $@
-	$(AR) cq $@ arch/arm/sysstubs/*.o
-	$(RANLIB) $@
-
-archclean:
-	-rm -rf arch/arm/sysstubs
-	-rm -f arch/arm/sysstubs.a

usr/klibc/arch/arm/syscall.S
@@ -15,6 +15,7 @@ __syscall_common:
 #ifdef __ARM_EABI__
 	ldr	r4, [sp,#16]
 	ldr	r5, [sp,#20]
+	ldr	r7, [lr]
 	swi	0
 #endif
         cmn     r0, #4096
@@ -34,8 +35,11 @@ __syscall_common:
 	.thumb_func
 	.balign	2
 __syscall_common:
+	mov	r7, lr
 	ldr	r4, [sp,#16]
+	sub	r7, #1		/* Remove the Thumb bit */
 	ldr	r5, [sp,#20]
+	ldrh	r7, [r7]
 	swi	0
 	ldr	r1, 2f
 	cmp	r0, r1

johnny-mnemonic/klibc-ia64

commit 259b87d50457760b804ef8be8a7905afa4ca283f

[klibc] fix arm stub alignment
By default perl silently drops writes to undefined file handles. Thus
".balign 4" is skipped in generated ARM stubs because the generator
contains:
  open(OUT, '>', "${outputdir}/${fname}.S");
  print  OUT "#include <asm/unistd.h>\n";
  ...
  print  out "	.balign	4\n";

Write to the generator's OUT filehandle rather than "out".

The result of this change for ARM stubs (e.g.,
./usr/klibc/syscalls/recvmsg.S):
  --- /tmp/before      2026-01-31 15:59:58.511500983 -0800
  +++ /tmp/after      2026-01-31 16:01:28.383070634 -0800
  @@ -13,6 +13,7 @@
          swi     # __NR_recvmsg
          b       __syscall_common
   #else /* __ARM_EABI__ */
  +       .balign 4
   recvmsg:
          stmfd   sp!,{r4,r5,r7,lr}
          bl      __syscall_common

Fixes: bd56c54ae409 ("[klibc] ARM: More EABI and Thumb fixes.  Make the syscall stubs smaller.")
Signed-off-by: Greg Thelen <gthelen@google.com>
Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

usr/klibc/arch/arm/sysstub.ph
@@ -33,7 +33,7 @@ sub make_sysstub($$$$$@) {
     print  OUT "#else /* __ARM_EABI__ */\n";
 
     # ARM EABI version
-    print  out "	.balign	4\n";
+    print  OUT "	.balign	4\n";
     print  OUT "${fname}:\n";
     print  OUT "	stmfd	sp!,{r4,r5,r7,lr}\n";
     print  OUT "	bl	__syscall_common\n";